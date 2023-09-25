f503ddc2b7fa4ac82bbc401d79a204dc00b27dcb
Byakuya91 <nganti91@yahoo.com>
2023-09-25 14:09:34 -0400

    I've successfully created the scaffolding for the first half of the field. I will proceed with the second part with the markup and tackle the custom form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+// ! REACT IMPORTS
 import React from "react";
+
+// ! LIBRARY IMPORTS
+import { FaClipboard } from "react-icons/fa";
 
 // ? GOAL: CREATE A PASSWORD GENERATOR THAT DOES THE FOLLOWING:
 // ? 1) generate a password with minimum six characters for length for varying passwords
@@ -6,8 +10,36 @@ import React from "react";
 // ? 3) Copy and paste the  generated password for increased ease
 // ? 4) Notifications to tell me if the password is copied, and have at least ONE option checked.
 
+// TODO: create the section and form for the password generator
+// TODO: create custom form for password generation
+// TODO: create functionality to randomly generate the password
+
 function App() {
-  return <div>App</div>;
+  return (
+    <section>
+      <div className="container">
+        <form id="pg-form">
+          <div className="result">
+            <input
+              type="text"
+              id="result"
+              placeholder="Min of 6 chars."
+              readOnly
+            />
+            <div className="clipboard">
+              <FaClipboard></FaClipboard>
+            </div>
+          </div>
+          <div>
+            <div className="field">
+              <label htmlFor="length">Length</label>
+              <input type="number" id="length" min={6} max={15} />
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default App;

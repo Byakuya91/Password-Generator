1844b90df2b6c00ee0e0ff392ae657bc67f7542e
Byakuya91 <nganti91@yahoo.com>
2023-09-25 19:36:09 -0400

    I've managed to fix the uncaught typeError by adding Arrow functions to prevent the functions from being ran each time the component rendered.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { FaClipboard } from "react-icons/fa";
 import { useForm } from "./useForm";
 
 // ! OTHER IMPORTS
-import { getRandomChar, getSpecialSymbol, utils } from "./utils";
+import { getRandomChar, getSpecialSymbol } from "./utils";
 
 // ? GOAL: CREATE A PASSWORD GENERATOR THAT DOES THE FOLLOWING:
 // ? 1) generate a password with minimum six characters for length for varying passwords
@@ -14,8 +14,9 @@ import { getRandomChar, getSpecialSymbol, utils } from "./utils";
 // ? 3) Copy and paste the  generated password for increased ease
 // ? 4) Notifications to tell me if the password is copied, and have at least ONE option checked.
 
-// TODO: create the section and form to control the inputs generator(ONGOING)
-// TODO: create functionality to randomly generate the password
+// TODO: create the section and form to control the inputs generator(DONE)
+// TODO: create functionality to randomly generate the password(DONE)
+// TODO: Create handle Form submit
 
 function App() {
   // State variables
@@ -37,26 +38,61 @@ function App() {
   const fieldsArray = [
     {
       field: values.capital,
-      getChar: getRandomChar(65, 90),
+      getChar: () => getRandomChar(65, 90),
     },
     {
       field: values.small,
-      getChar: getRandomChar(97, 122),
+      getChar: () => getRandomChar(97, 122),
     },
     {
       field: values.number,
-      getChar: getRandomChar(48, 57),
+      getChar: () => getRandomChar(48, 57),
     },
     {
       field: values.symbol,
-      getChar: getSpecialSymbol(),
+      getChar: () => getSpecialSymbol(),
     },
   ];
+
+  // submitting the form
+  const handleOnSubmit = (e) => {
+    // prevent the page from reloading
+    e.preventDefault();
+
+    // Generating the password
+
+    // storing the password
+    let generatedPassword = "";
+
+    // grab the fields that only have the checked marks in it.
+
+    const checkedFields = fieldsArray.filter(({ field }) => field);
+
+    // loop through the results
+    for (let i = 0; i < values.length; i++) {
+      // grabbing an index
+      const index = Math.floor(Math.random() * checkedFields.length);
+
+      // get the letter with the index
+      const letter = checkedFields[index]?.getChar();
+
+      // Once a letter is grabbed
+
+      if (letter) {
+        generatedPassword += letter;
+      }
+
+      //  Once the password is generated
+      if (generatedPassword) {
+        setResult(generatedPassword);
+      }
+    }
+  };
 
   return (
     <section>
       <div className="container">
-        <form id="pg-form">
+        <form id="pg-form" onSubmit={handleOnSubmit}>
           <div className="result">
             <input
               type="text"

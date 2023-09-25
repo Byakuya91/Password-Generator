32694439f61652b1e1fc5a687ee4a149df375be8
Byakuya91 <nganti91@yahoo.com>
2023-09-25 13:53:18 -0400

    I've further prepped the boilerplate and will move to establishing the HTML structure and styles which I have created, before going onto creating a custom hook for the form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+
+// ? GOAL: CREATE A PASSWORD GENERATOR THAT DOES THE FOLLOWING:
+// ? 1) generate a password with minimum six characters for length for varying passwords
+// ? 2) Include modifications for capital letters, symbols, and numbers for varying passwords
+// ? 3) Copy and paste the  generated password for increased ease
+// ? 4) Notifications to tell me if the password is copied, and have at least ONE option checked.
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <div>App</div>;
 }
 
-export default App
+export default App;
